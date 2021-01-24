00966e3a2a87926f687bf3d70aac143f91af53ea
davidh <iq.davidh@gmail.com>
2021-01-24 17:03:02 -0600

    ya tenemos el loop

--- src/fnGetDataTxtFile.js
+++ src/fnGetDataTxtFile.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
+const  path = require('path');
+
 
 /***
  * A partir del txt lo convertimois en un modelo rooom
  */
 const fnGetDataTxtFile =()=>{
+  const p = path.join(__dirname, '../', 'room.txt');
   //convertir cada linea en un array de celdas
-  const room= fs.readFileSync('../room.txt', 'utf8')
+  const room= fs.readFileSync(p, 'utf8')
   const lineas = room.split("\n")
   
   const listaClean =lineas.map( item=>{
