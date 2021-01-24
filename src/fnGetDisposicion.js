145ab7815de16b9cbd3f97c4860418b555faebfd
davidh <iq.davidh@gmail.com>
2021-01-24 16:02:15 -0600

    ya ten4emos el loop para agregar foco

--- src/fnGetDisposicion.js
+++ src/fnGetDisposicion.js
@@ -10,6 +10,14 @@ class Celda {
     this.indexLum = 0; //<---- num de celdas total iluminadas que se optienen al poner el foco en esta posicion
   }
   
+  setIndexLum(v) {
+    this.indexLum = v;
+  }
+  
+  setFoco() {
+    this.hasFoco = true;
+  }
+  
 }
 
 const fnConvertirRoom = (room) => {
@@ -89,12 +97,61 @@ function getNumCeldasEnColumnaIluminadas(listaRows, i, j) {
   return numColsRequiere;
 }
 
+function getNumCeldasEnRowIluminadas(listaRows, i, j) {
+  
+  const numRows = listaRows.length;
+  
+  //buscar en eje x / columna
+  let indexRowIni = j;
+  let isRowIniCompleted = false;
+  while (!isRowIniCompleted) {
+    if (indexRowIni > 0 && listaRows[indexRowIni - 1][j].isEspacio) {
+      if (indexRowIni > 0) {
+        indexRowIni--;
+        const celdaContinua = listaRows[indexRowIni][j];
+        isRowIniCompleted = !celdaContinua.isEspacio;
+      } else {
+        isRowIniCompleted = true;
+      }
+    } else {
+      isRowIniCompleted = true;
+    }
+    
+  }
+  
+  let indexRowFin = j;
+  let isRowFinCompleted = false;
+  while (!isRowFinCompleted) {
+    if (indexRowFin < (numRows - 1) && listaRows[indexRowFin + 1][j].isEspacio) {
+      if (indexRowFin < numRows) {
+        indexRowFin++;
+        const celdaContinua = listaRows[indexRowFin][j];
+        isRowFinCompleted = !celdaContinua.isEspacio;
+      } else {
+        isRowFinCompleted = true;
+      }
+    } else {
+      isRowFinCompleted = true;
+    }
+    
+  }
+  
+  //numero de celdas en columna que ilum,inaria un foco
+  
+  //ver si el rango de columnas  de celdas requiere  iluminacion
+  let numRowsRequiere = 0;
+  for (let index = indexRowIni; index <= indexRowFin; index++) {
+    if (listaRows[index][j].isRequiereIluminacion) {
+      numRowsRequiere++;
+    }
+  }
+  return numRowsRequiere;
+}
+
 const fnGetDisposicion = (room) => {
   
   //convertir room en celdas
   const {listaRows, listaCeldas} = fnConvertirRoom(room);
-  
-
   
   
   let isCompleted = false;
@@ -104,25 +161,36 @@ const fnGetDisposicion = (room) => {
     iteracion++;
     
     //agregar un foco
-    {
-      //calcular el index de Ilum a cada celda y ordenarlas de forma desc
-      for (let i = 0; i < listaRows.length; i++) {
-        for (let j = 0; j < listaRows[i].length; j++) {
-          
-          let celda = listaRows[i][j];
-          
-          if (celda.isEspacio) {
-            let index = getNumCeldasEnColumnaIluminadas(listaRows, i, j);
-  
-            //ilumnar por renmglon ----------------------------
-            
-            const x=0;
-          }
-          
-          
+    
+    //calcular el index de Ilum a cada celda y ordenarlas de forma desc
+    for (let i = 0; i < listaRows.length; i++) {
+      for (let j = 0; j < listaRows[i].length; j++) {
+        
+        let celda = listaRows[i][j];
+        
+        if (celda.isEspacio) {
+          let numCeldasCol = getNumCeldasEnColumnaIluminadas(listaRows, i, j);
+          let numCeldasRow = getNumCeldasEnRowIluminadas(listaRows, i, j);
+          celda.setIndexLum(numCeldasCol + numCeldasRow);
         }
       }
     }
+    
+    //ORdener por indexLum para optimizar el efecto de un foco
+    const listaSort = [...listaCeldas]
+      .filter(k => k.isRequiereIluminacion)
+      .sort((a, b) => {
+        if (a.indexLum === b.indexLum) {
+          return 0;
+        }
+        return a.indexLum > b.indexLum
+          ? -1
+          : 1
+      });
+    
+    
+    //agregar un foco a la celda con max indexLum
+    listaSort[0].setFoco();
     
     
     let isCompleteted = fnGetIsRoomIluminado(listaCeldas)
